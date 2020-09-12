949f4c729d02716f0e172fee412db2b8d1fbb029
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-09-12 09:42:10 +0600

    context api used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
 import Login from './components/Login/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import Book from './components/Book/Book';
+
+export const UserContext = createContext()
+
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <p>Name: {loggedInUser.name}</p>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" >
+          <Home />
+        </Route>
+        <Route path="/home" >
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/book/:bedType">
+          <Book />
+        </Route>
+      </Switch>
+    </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
-
+import {UserContext} from '../../App';
 
 
 
 const Login = () => {
-    firebase.initializeApp(firebaseConfig);
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+
+    if (firebase.apps.length === 0) {
+            firebase.initializeApp(firebaseConfig);
+    }
+    
     const handleGoogleSignIn = () => {
         
         const provider = new firebase.auth.GoogleAuthProvider();
 
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            console.log(user)
-          }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+        firebase.auth().signInWithPopup(provider)
+        .then(res => {
+            const {displayName, email, photoURL} = res.user;
+            const signedInUser = {name: displayName, email, photoURL}
+            setLoggedInUser(signedInUser)
+          }).catch( err => {
+            console.log(err.message)
           });
     }
     return (
