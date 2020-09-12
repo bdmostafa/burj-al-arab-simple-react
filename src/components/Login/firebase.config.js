9f4c0e47c02b5acf1fe3c06a1928087f432c0108
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-09-12 09:00:49 +0600

    firebase google sign in

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7UrRCxHjOqgCK3eOthQwP3mj0IbACk8c",
+    authDomain: "burj-al-arab2020.firebaseapp.com",
+    databaseURL: "https://burj-al-arab2020.firebaseio.com",
+    projectId: "burj-al-arab2020",
+    storageBucket: "burj-al-arab2020.appspot.com",
+    messagingSenderId: "41246761690",
+    appId: "1:41246761690:web:1754fe92b5a76a1cadbbe3"
+  };
+
+  export default firebaseConfig;
